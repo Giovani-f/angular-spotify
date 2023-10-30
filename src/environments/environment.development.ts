11d9ce5731eb503eab0ea6ea3ea021bdb06fa61a
Giovani Fernandes <gifernandes105@gmail.com>
2023-10-30 17:41:50 -0300

    chore: config tailwindcss

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,17 @@
+export const environment = {};
+
+export const SpotifyConfig = {
+  clientId: "4613e6be0f5e453399d1b23207e96c3a",
+  authEndpoint: "https://accounts.spotify.com/authorize",
+  redirectUri: "http://localhost:4200/login/",
+  scopes: [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+    "user-library-read",
+    "playlist-read-private",
+    "playlist-read-collaborative"
+  ]
+}
