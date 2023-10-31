9dcc5b3cdb47e6b43eebee18766ca1d870587f90
Giovani Fernandes <gifernandes105@gmail.com>
2023-10-31 09:58:20 -0300

    feat: player module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,4 +11,9 @@ export const AppRoutes: Routes = [
     loadChildren: () =>
       import('./pages/login/login.module').then(m => m.LoginModule),
   },
+  {
+    path: 'player',
+    loadChildren: () =>
+      import('./pages/player/player.module').then(m => m.PlayerModule),
+  },
 ];
