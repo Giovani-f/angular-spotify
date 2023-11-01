bcd05aebf64514c4970944a84443f0e5acc26503
Giovani Fernandes <gifernandes105@gmail.com>
2023-11-01 08:46:21 -0300

    style: sidebar navigation

--- src/app/pages/player/player.module.ts
+++ src/app/pages/player/player.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { PlayerComponent } from './player.component';
 import { Router, RouterModule } from '@angular/router';
 import { PlayerRoutes } from './player.routes';
+import { SidebarNavigationComponent } from 'src/app/components/sidebar-navigation/sidebar-navigation.component';
 
 @NgModule({
-  declarations: [PlayerComponent],
+  declarations: [PlayerComponent, SidebarNavigationComponent],
   imports: [CommonModule, RouterModule.forChild(PlayerRoutes)],
 })
 export class PlayerModule {}
